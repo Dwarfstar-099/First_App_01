47afe82b6579072a9529a7e4883ccd7fb191d762
Dwarfstar-099 <code2change.gourav@gmail.com>
2022-05-29 18:09:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basic_app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.basic_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.jar.Attributes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
